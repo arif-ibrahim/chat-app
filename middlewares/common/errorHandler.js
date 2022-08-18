24b9a8d7a942af3288a8ad70e6f656e638cfe54b
Arif Ibrahim <ibrahim.jatri@gmail.com>
2022-08-18 22:10:56 +0600

    error message updated

--- middlewares/common/errorHandler.js
+++ middlewares/common/errorHandler.js
@@ -2,7 +2,7 @@ const createError = require("http-errors");
 
 // 404 not found handler
 function notFoundHandler(req, res, next) {
-  next(createError(404, "Your requested content was not found!"));
+  next(createError(404, "Your requested content was not found!!"));
 }
 
 // default error handler
